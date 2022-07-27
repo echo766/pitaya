daa1f48d138f58f21d0854879fc3cf9609aa54e9
caochunxi <caochunxi@hotmail.com>
2022-07-27 13:51:46 +0800

    add consts

--- constants/const.go
+++ constants/const.go
@@ -85,6 +85,9 @@ const (
 	// GRPCPortKey is the key for grpc port on server metadata
 	GRPCPortKey = "grpcPort"
 
+	// ServerID is the key for server id on server metadata
+	ServerID = "serverID"
+
 	// GRPCExternalPortKey is the key for grpc external port on server metadata
 	GRPCExternalPortKey = "grpc-external-port"
 
